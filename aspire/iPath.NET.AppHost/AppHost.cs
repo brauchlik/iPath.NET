878ed6db52ff2d63584e8271ad76f8bd2e9239f6
kurt <kb@basysdata.ch>
2026-01-20 15:58:01 +0100

    HTML Editor is incompatible with. Removing integrated Ollama

--- aspire/iPath.NET.AppHost/AppHost.cs
+++ aspire/iPath.NET.AppHost/AppHost.cs
@@ -6,18 +6,21 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres");
 var postgresdb = postgres.AddDatabase("postgresdb");
 
+/*
 var ollama = builder.AddOllama("ollama")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume()
     .WithOpenWebUI();
 var mistral = ollama.AddModel("mistral", "mistral");
-
+*/
 
 var ipath = builder.AddProject<iPath_Blazor_Server>("ipath-server")
     .WithReference(postgresdb)
-    .WaitFor(postgresdb)
+    .WaitFor(postgresdb);
+/*
     .WithReference(ollama)
     .WaitFor(ollama);
+*/
 
 var app = builder.Build();
 app.Run();
